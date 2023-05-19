2a0c8ed8240e548d77cff2e3fcb82f4a9d4e5e8e
Saul Shanabrook <s.shanabrook@gmail.com>
2023-05-19 00:55:24 -0400

    First go at github plugin

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# ChatGPT plugins quickstart
+# OpenAI Plugin for GitHub API
 
-Get a todo list ChatGPT plugin up and running in under 5 minutes using Python. If you do not already have plugin developer access, please [join the waitlist](https://openai.com/waitlist/plugins).
+This is an OpenAI plugin that allows you to query the GitHub API by proxying all traffic to GitHub and returning their OpenAPI spec.
+If you do not already have plugin developer access, please [join the waitlist](https://openai.com/waitlist/plugins).
 
 ## Setup
 
@@ -18,14 +19,10 @@ python main.py
 
 Once the local server is running:
 
-1. Navigate to https://chat.openai.com. 
+1. Navigate to https://chat.openai.com.
 2. In the Model drop down, select "Plugins" (note, if you don't see it there, you don't have access yet).
 3. Select "Plugin store"
 4. Select "Develop your own plugin"
 5. Enter in `localhost:5003` since this is the URL the server is running on locally, then select "Find manifest file".
 
-The plugin should now be installed and enabled! You can start with a question like "What is on my todo list" and then try adding something to it as well! 
-
-## Getting help
-
-If you run into issues or have questions building a plugin, please join our [Developer community forum](https://community.openai.com/c/chat-plugins/20).
+The plugin should now be installed and enabled! You can start with a question like "What repos does saulshanabrook contribute to?"
--- main.py
+++ main.py
@@ -1,56 +1,92 @@
-import json
+from pathlib import Path
 
 import quart
 import quart_cors
+import requests
+import yaml
 from quart import request
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
+@app.route("/<path:path>", methods=["GET", "POST", "PUT", "DELETE"])
+async def proxy_request(path):
+    url = f"https://api.github.com/{path}"
+    response = requests.request(
+        method=request.method,
+        url=url,
+        headers=dict(request.headers),
+        data=await request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False,
+    )
+    return quart.Response(
+        response=response.content,
+        status=response.status_code,
+        headers=dict(response.headers),
+    )
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
-
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
 
 @app.get("/logo.png")
 async def plugin_logo():
-    filename = 'logo.png'
-    return await quart.send_file(filename, mimetype='image/png')
+    filename = "logo.png"
+    return await quart.send_file(filename, mimetype="image/png")
+
 
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
-    host = request.headers['Host']
     with open("./.well-known/ai-plugin.json") as f:
         text = f.read()
         return quart.Response(text, mimetype="text/json")
 
+
+CACHE_FILE = Path("github_api.yaml")
+
+OPENAPI_PLUGIN = Path("openapi.yaml")
+
+
 @app.get("/openapi.yaml")
 async def openapi_spec():
-    host = request.headers['Host']
-    with open("openapi.yaml") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/yaml")
+    if OPENAPI_PLUGIN.exists():
+        text = OPENAPI_PLUGIN.read_text()
+    else:
+        host = request.headers["Host"]
+        if CACHE_FILE.exists():
+            text = CACHE_FILE.read_text()
+        else:
+            url = "https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.yaml"
+            response = requests.get(url)
+            text = response.text
+            CACHE_FILE.write_text(response.text)
+        github_text = text.replace("https://api.github.com", "http://localhost:5003").replace(
+            "http://api.github.com", "http://localhost:5003"
+        )
+        github_spec = yaml.safe_load(github_text)
+        spec = {
+            "openapi": github_spec["openapi"],
+            "info": {
+                "title": "Unofficial Github Plugin",
+                "description": "Unofficial Github Plugin for OpenAI Chat",
+                "version": "v1",
+            },
+            "servers": [{"url": "http://localhost:5003"}],
+            "paths": dict(list(github_spec["paths"].items())[:200]),
+            "components": {"schemas": dict(list(github_spec["components"]["schemas"].items())[:200])},
+        }
+        text = yaml.dump(spec)
+        # Switch all lines that end in - null to - "null"
+        # {"detail":"Error setting localhost plugin: {\"validation_errors\":[{\"loc\":[\"enum\",9],\"msg\":\"none is not an allowed value\",\"type\":\"type_error.none.not_allowed\"}],\"message\":\"1 validation error for StringSchema\\nenum -> 9\\n  none is not an allowed value (type=type_error.none.not_allowed)\"}"}
+        text = "\n".join(
+            line if not line.strip().endswith("- null") else line.replace("- null", '- "null"')
+            for line in text.split("\n")
+        )
+        OPENAPI_PLUGIN.write_text(text)
+    return quart.Response(text, mimetype="text/yaml")
+
 
 def main():
     app.run(debug=True, host="0.0.0.0", port=5003)
+
 
 if __name__ == "__main__":
     main()
